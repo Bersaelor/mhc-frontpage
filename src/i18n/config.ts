17004df61ed528f29a0b0578b4a5f0a808caaaf9
Konrad Feiler <konrad@mathheartcode.com>
2023-05-03 23:34:36 +0200

    do not use language detector

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,16 +1,13 @@
 import i18next from 'i18next';
 import HttpApi from 'i18next-xhr-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
 const i18n = i18next
   .use(HttpApi)
-  .use(LanguageDetector)
   .init({
     load: 'languageOnly',
     fallbackLng: 'en',
     preload: ['en', 'de'],
     ns: 'translations',
     defaultNS: 'translations',
-    fallbackNS: false,
     debug: true,
     detection: {
       order: ['querystring', 'navigator', 'htmlTag'],
